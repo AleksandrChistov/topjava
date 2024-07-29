69280dfd8e428db67b14c0b8ef0652a8135581fc
Александр Чистов <aleksandrchistov.ru@yandex.ru>
2024-07-29 20:07:19 +0700

    Get rid of conversion of Meal from MealDao

--- src/main/java/ru/javawebinar/topjava/util/MealDao.java
+++ src/main/java/ru/javawebinar/topjava/util/MealDao.java
@@ -1,18 +1,14 @@
 package ru.javawebinar.topjava.util;
 
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.MealTo;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class MealDao {
-    private static final int CALORIES_PER_DAY = 2000;
+    public static final int CALORIES_PER_DAY = 2000;
 
     private final List<Meal> meals = Arrays.asList(
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
@@ -24,16 +20,7 @@ public class MealDao {
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
     );
 
-    public List<MealTo> get() {
-        Map<LocalDate, Integer> caloriesPerDayMap = meals.stream()
-                .collect(Collectors.groupingBy(
-                                Meal::getDate,
-                                Collectors.summingInt(Meal::getCalories)
-                        )
-                );
-
-        return meals.stream()
-                .map(meal -> MealsUtil.createTo(meal, caloriesPerDayMap.get(meal.getDate()) > CALORIES_PER_DAY))
-                .collect(Collectors.toList());
+    public List<Meal> get() {
+        return meals;
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -2,12 +2,14 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import ru.javawebinar.topjava.util.MealDao;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -18,7 +20,7 @@ public class MealServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("open meals");
-        request.setAttribute("meals", mealDao.get());
+        request.setAttribute("meals", MealsUtil.filteredByStreams(mealDao.get(), LocalTime.MIN, LocalTime.MAX, MealDao.CALORIES_PER_DAY));
         request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
