c2e3db6ebbf4b033906edd704934b13705b3fdcc
Александр Чистов <aleksandrchistov.ru@yandex.ru>
2024-07-29 09:52:00 +0700

    Create table with meals

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,21 +1,43 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
+    private static final int CALORIES_PER_DAY = 2000;
+    private List<MealTo> list = getHardcodedMeals();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("open meals");
+        request.setAttribute("meals", list);
         request.getRequestDispatcher("/meals.jsp").forward(request, response);
+    }
+
+    private List<MealTo> getHardcodedMeals() {
+        List<Meal> meals = new ArrayList<>();
+        meals.add(new Meal(LocalDateTime.of(2024, Month.JULY, 20, 19, 0), "Eggs", 500));
+        meals.add(new Meal(LocalDateTime.of(2024, Month.JULY, 20, 13, 15), "Bread", 800));
+        meals.add(new Meal(LocalDateTime.of(2024, Month.JULY, 20, 7, 0), "Sausages", 1000));
+        meals.add(new Meal(LocalDateTime.of(2024, Month.MAY, 1, 7, 30), "Soup", 350));
+        meals.add(new Meal(LocalDateTime.of(2024, Month.MAY, 1, 12, 30), "Bread", 800));
+        meals.add(new Meal(LocalDateTime.of(2024, Month.APRIL, 5, 9, 45), "Pork chop", 1500));
+        return MealsUtil.filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(19, 1), CALORIES_PER_DAY);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,11 +1,34 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <html lang="ru">
 <head>
     <title>Meals</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h3><a href="index.html">Home</a></h3>
-<hr>
-<h2>Meals</h2>
+    <h3><a href="index.html">Home</a></h3>
+    <hr>
+    <h2>Meals</h2>
+    <table>
+        <thead>
+        <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Calories</th>
+        </tr>
+        </thead>
+        <tbody>
+        <jsp:useBean id="meals" scope="request" type="java.util.List<ru.javawebinar.topjava.model.MealTo>"/>
+        <c:forEach var="meal" items="${meals}">
+            <tr style="color: ${meal.excess ? 'red' : 'green'}">
+                <fmt:parseDate value="${meal.dateTime}" pattern="yyyy-MM-dd'T'HH:mm" var="parsedDateTime" type="both"/>
+                <td><fmt:formatDate pattern="dd.MM.yyyy HH:mm" value="${parsedDateTime}"/></td>
+                <td>${meal.description}</td>
+                <td>${meal.calories}</td>
+            </tr>
+        </c:forEach>
+        </tbody>
+    </table>
 </body>
 </html>
