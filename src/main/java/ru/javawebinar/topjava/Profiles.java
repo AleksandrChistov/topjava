ca5b6341864c6faba7db32c17d38c9a83c211520
Александр Чистов <aleksandrchistov.ru@yandex.ru>
2024-09-01 20:40:02 +0700

    11_14_vds_restrict_update patch

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -12,10 +12,11 @@ public class Profiles {
 
     public static final String
             POSTGRES_DB = "postgres",
-            HSQL_DB = "hsqldb";
+            HSQL_DB = "hsqldb",
+            VDS = "vds";
 
     public static final String ACTIVE_DB = HSQL_DB;
-    //  Get DB profile depending of DB driver in classpath
+    //  Get DB profile depending on DB driver in classpath
     public static String getActiveDbProfile() {
         if (ClassUtils.isPresent("org.postgresql.Driver", null)) {
             return POSTGRES_DB;
